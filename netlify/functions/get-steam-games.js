f0207b06babe1f28504c73faaaeb76f0c28932f4
Patrick Randell <prandell@student.unimelb.edu.au>
2022-05-05 08:15:58 +1000

    change to import for netlify functions

--- netlify/functions/get-steam-games.js
+++ netlify/functions/get-steam-games.js
@@ -1,5 +1,5 @@
+import fetch from 'node-fetch'
 require('dotenv').config()
-const fetch = require('node-fetch')
 
 const API_ENDPOINT = `https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?steamid=76561199068492201&count=3&key=${process.env.REACT_APP_STEAM_API_KEY}`
 
